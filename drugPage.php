b04c6ea6b2272a73a615b93574f1f6fa5ab0d34e
David Streid <dcs2153@columbia.edu>
2015-04-03 12:03:57 -0400

    Reformatted mwasData for parsing to successfully read in drug data

--- drugPage.php
+++ drugPage.php
@@ -28,52 +28,45 @@
         </style>
     </head>
     <body>
-        <script type="text/javascript">    
-    
-            var drugList = [
-                
-                <?php
-                    $drugList = array();
-                    $myfile = fopen("MWAS_data.txt", "r") or die("Unable to open file!");
-                    if ($myfile) {
-
-                        while (($line = fgets($myfile)) !== false) {
-                            $x = explode(chr(9), $line); // Tab = Ascii 9  
-                            $RANK = $x[0];
-                            $MIN_DETECTABLE_RR = $x[1];
-                            $SOURCE_ID = $x[2];
-                            $DRUG_CONCEPT_ID = $x[3];
-                            $CONDITION_CONCEPT_ID = $x[4];
-                            $GROUND_TRUTH = $x[5];
-                            $LOGRR = $x[6];
-                            $LOGLB95RR = $x[7];
-                            $P = $x[8];
-
-                            $DRUG = array(
-                                $RANK,
-                                $MIN_DETECTABLE_RR,
-                                $SOURCE_ID,
-                                $DRUG_CONCEPT_ID,
-                                $CONDITION_CONCEPT_ID,
-                                $GROUND_TRUTH,
-                                $LOGRR,
-                                $LOGLB95RR,
-                                $P
-                            );
-
-                            array_push($drugList, $DRUG);
+        <script type="text/javascript"> 
+                var dL = [
+                    <?php
+                        $drugList = array();
+                        $myfile = fopen("mwasData.txt", "r") or die("Unable to open file!"); // pre-processed (first 9 columns)
+                        if ($myfile) {
+                            while (($line = fgets($myfile)) !== false) {
+                                $x = explode(chr(9), $line); // Tab = Ascii 9  
+                                    $RANK = $x[0];
+                                    $MIN_DETECTABLE_RR = $x[1];
+                                    $SOURCE_ID = $x[2];
+                                    $DRUG_CONCEPT_ID = $x[3];
+                                    $CONDITION_CONCEPT_ID = $x[4];
+                                    $GROUND_TRUTH = $x[5];
+                                    $LOGRR = $x[6];
+                                    $LOGLB95RR = $x[7];
+                                    $P = $x[8];
+                                $DRUG = array(
+                                    $RANK,
+                                    $MIN_DETECTABLE_RR,
+                                    $SOURCE_ID,
+                                    $DRUG_CONCEPT_ID,
+                                    $CONDITION_CONCEPT_ID,
+                                    $GROUND_TRUTH,
+                                    $LOGRR,
+                                    $LOGLB95RR,
+                                    $P
+                                );
+                                array_push($drugList, $DRUG);
+                            }
+                        } else {
+                            echo "Error reading File";
                         }
-
-                    } else {
-                        echo "Error reading File";
-                    }
-                    fclose($myfile);
-                    echo json_encode($drugList);
-                ?>
-                
-                
-            ];  
+                        fclose($myfile);
+                        echo json_encode($drugList);
+                    ?>    
+                ];
             
+                console.log(dL);
             
         </script>
     </body>
